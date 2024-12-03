a998ba336e6fdca30e264e20b9277c4642020332
Mateo Kos <80221816+LeKosfrancuz@users.noreply.github.com>
2024-12-04 00:43:21 +0100

    Izvadio logiku računanja brzine motora u funkciju

--- main.py
+++ main.py
@@ -10,9 +10,18 @@ def on_forever():
         radio.send_value("stop", 0)
         basic.clear_screen()
     
+    (l_motor, r_motor) = calculate_motor_speed()
+    
+    radio.send_value("set_l_motor", l_motor)
+    radio.send_value("set_r_motor", r_motor)
+    print("Left motor: " + l_motor)
+    print("Right motor: " + r_motor)
+    
+
+def calculate_motor_speed():
     pitch = input.rotation(Rotation.PITCH)
     roll  = input.rotation(Rotation.ROLL)
-    almost_zero_deg_tolerance = 15
+    almost_zero_deg_tolerance = 25
 
     #if Math.abs(pitch) > 95 or Math.abs(roll) > 95:
     #    basic.show_icon(IconNames.NO)
@@ -25,32 +34,44 @@ def on_forever():
     a_pitch = Math.abs(pitch)
     a_roll  = Math.abs(roll)
 
-    if a_pitch/a_roll < 1.5 and a_pitch/a_roll >= 0.5:
-        if pitch > 0 and roll > 0:
-            basic.show_arrow(ArrowNames.SOUTH_EAST)
-        if pitch > 0 and roll < 0:
-            basic.show_arrow(ArrowNames.SOUTH_WEST)
-        if pitch < 0 and roll > 0:
-            basic.show_arrow(ArrowNames.NORTH_EAST)
-        if pitch < 0 and roll < 0:
-            basic.show_arrow(ArrowNames.NORTH_WEST)
+    l_motor = 0
+    r_motor = 0
 
-    if a_pitch > a_roll:
+    if a_pitch/a_roll < 1.5 and a_pitch/a_roll >= 0.5:
+        # Ako je jedan od smjerova preko tolerancije, dovoljno je za kretanje
+        if a_pitch > almost_zero_deg_tolerance or a_roll > almost_zero_deg_tolerance:
+            if pitch > 0 and roll > 0:
+                basic.show_arrow(ArrowNames.SOUTH_EAST)
+            if pitch > 0 and roll < 0:
+                basic.show_arrow(ArrowNames.SOUTH_WEST)
+            if pitch < 0 and roll > 0:
+                basic.show_arrow(ArrowNames.NORTH_EAST)
+            if pitch < 0 and roll < 0:
+                basic.show_arrow(ArrowNames.NORTH_WEST)
+            l_motor = -1*pitch/180 + roll/180
+            r_motor = -1*pitch/180 - roll/180
+    elif a_pitch > a_roll:
         if a_pitch > almost_zero_deg_tolerance:
             if pitch > 0: # Nagnut nazad
+                l_motor = -1*a_pitch/180
+                r_motor = l_motor
                 basic.show_arrow(ArrowNames.SOUTH)
-                print("Nagnut nazad: "+a_pitch+"°")
             elif pitch < 0: # Nagnut naprijed
+                l_motor = a_pitch/180
+                r_motor = l_motor
                 basic.show_arrow(ArrowNames.NORTH)
-                print("Nagnut naprijed: "+a_pitch+"°")
     else:
         if a_roll > almost_zero_deg_tolerance:
             if roll > 0: # Nagnut desno
+                l_motor = roll/180
+                r_motor = -1*l_motor
                 basic.show_arrow(ArrowNames.EAST)
-                print("Nagnut desno: "+a_roll+"°")
             elif roll < 0: # Nagnut lijevo
+                l_motor = roll/180
+                r_motor = -1*l_motor
                 basic.show_arrow(ArrowNames.WEST)
-                print("Nagnut lijevo: "+a_roll+"°")
+
+    return (l_motor, r_motor)
 
 basic.forever(on_forever)
 
--- main.ts
+++ main.ts
@@ -1,6 +1,78 @@
 let radio_group = 4
 radio.setGroup(radio_group)
 basic.showNumber(radio_group)
+function calculate_motor_speed(): number[] {
+    let pitch = input.rotation(Rotation.Pitch)
+    let roll = input.rotation(Rotation.Roll)
+    let almost_zero_deg_tolerance = 25
+    // if Math.abs(pitch) > 95 or Math.abs(roll) > 95:
+    //     basic.show_icon(IconNames.NO)
+    //     return
+    // else:
+    //     basic.clear_screen()
+    console.log("Pitch: " + pitch + "°")
+    console.log("Roll: " + roll + "°")
+    let a_pitch = Math.abs(pitch)
+    let a_roll = Math.abs(roll)
+    let l_motor = 0
+    let r_motor = 0
+    if (a_pitch / a_roll < 1.5 && a_pitch / a_roll >= 0.5) {
+        //  Ako je jedan od smjerova preko tolerancije, dovoljno je za kretanje
+        if (a_pitch > almost_zero_deg_tolerance || a_roll > almost_zero_deg_tolerance) {
+            if (pitch > 0 && roll > 0) {
+                basic.showArrow(ArrowNames.SouthEast)
+            }
+            
+            if (pitch > 0 && roll < 0) {
+                basic.showArrow(ArrowNames.SouthWest)
+            }
+            
+            if (pitch < 0 && roll > 0) {
+                basic.showArrow(ArrowNames.NorthEast)
+            }
+            
+            if (pitch < 0 && roll < 0) {
+                basic.showArrow(ArrowNames.NorthWest)
+            }
+            
+            l_motor = -1 * pitch / 180 + roll / 180
+            r_motor = -1 * pitch / 180 - roll / 180
+        }
+        
+    } else if (a_pitch > a_roll) {
+        if (a_pitch > almost_zero_deg_tolerance) {
+            if (pitch > 0) {
+                //  Nagnut nazad
+                l_motor = -1 * a_pitch / 180
+                r_motor = l_motor
+                basic.showArrow(ArrowNames.South)
+            } else if (pitch < 0) {
+                //  Nagnut naprijed
+                l_motor = a_pitch / 180
+                r_motor = l_motor
+                basic.showArrow(ArrowNames.North)
+            }
+            
+        }
+        
+    } else if (a_roll > almost_zero_deg_tolerance) {
+        if (roll > 0) {
+            //  Nagnut desno
+            l_motor = roll / 180
+            r_motor = -1 * l_motor
+            basic.showArrow(ArrowNames.East)
+        } else if (roll < 0) {
+            //  Nagnut lijevo
+            l_motor = roll / 180
+            r_motor = -1 * l_motor
+            basic.showArrow(ArrowNames.West)
+        }
+        
+    }
+    
+    return [l_motor, r_motor]
+}
+
 basic.forever(function on_forever() {
     if (input.buttonIsPressed(Button.B)) {
         while (input.buttonIsPressed(Button.B)) {
@@ -11,64 +83,11 @@ basic.forever(function on_forever() {
         basic.clearScreen()
     }
     
-    let pitch = input.rotation(Rotation.Pitch)
-    let roll = input.rotation(Rotation.Roll)
-    let almost_zero_deg_tolerance = 15
-    // if Math.abs(pitch) > 95 or Math.abs(roll) > 95:
-    //     basic.show_icon(IconNames.NO)
-    //     return
-    // else:
-    //     basic.clear_screen()
-    console.log("Pitch: " + pitch + "°")
-    console.log("Roll: " + roll + "°")
-    let a_pitch = Math.abs(pitch)
-    let a_roll = Math.abs(roll)
-    if (a_pitch / a_roll < 1.5 && a_pitch / a_roll >= 0.5) {
-        if (pitch > 0 && roll > 0) {
-            basic.showArrow(ArrowNames.SouthEast)
-        }
-        
-        if (pitch > 0 && roll < 0) {
-            basic.showArrow(ArrowNames.SouthWest)
-        }
-        
-        if (pitch < 0 && roll > 0) {
-            basic.showArrow(ArrowNames.NorthEast)
-        }
-        
-        if (pitch < 0 && roll < 0) {
-            basic.showArrow(ArrowNames.NorthWest)
-        }
-        
-    }
-    
-    if (a_pitch > a_roll) {
-        if (a_pitch > almost_zero_deg_tolerance) {
-            if (pitch > 0) {
-                //  Nagnut nazad
-                basic.showArrow(ArrowNames.South)
-                console.log("Nagnut nazad: " + a_pitch + "°")
-            } else if (pitch < 0) {
-                //  Nagnut naprijed
-                basic.showArrow(ArrowNames.North)
-                console.log("Nagnut naprijed: " + a_pitch + "°")
-            }
-            
-        }
-        
-    } else if (a_roll > almost_zero_deg_tolerance) {
-        if (roll > 0) {
-            //  Nagnut desno
-            basic.showArrow(ArrowNames.East)
-            console.log("Nagnut desno: " + a_roll + "°")
-        } else if (roll < 0) {
-            //  Nagnut lijevo
-            basic.showArrow(ArrowNames.West)
-            console.log("Nagnut lijevo: " + a_roll + "°")
-        }
-        
-    }
-    
+    let [l_motor, r_motor] = calculate_motor_speed()
+    radio.sendValue("set_l_motor", l_motor)
+    radio.sendValue("set_r_motor", r_motor)
+    console.log("Left motor: " + l_motor)
+    console.log("Right motor: " + r_motor)
 })
 input.onLogoDown(function on_logo_down() {
     
